0683a9c6bf61193781abb3eb4fed5843d70dc702
Faizul Osman <faizulosmanrabby@gmail.com>
2022-10-26 09:33:55 +0600

    list updated

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get("/course/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const course = courses.find((c) => c.id === id);
+  res.send(course);
+});
+
 app.listen(port, () => {
   console.log("Programming Guru server comes form: ", port);
 });
