5fe49d59c5c0724869bac209a7e5bcc74f1de6b1
Faizul Osman <faizulosmanrabby@gmail.com>
2022-10-26 21:27:39 +0600

    response send for faq data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || "5000";
 app.use(cors());
 
 const courses = require("./data/courses.json");
+const faq = require("./data/faqData.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -20,6 +21,10 @@ app.get("/course/:id", (req, res) => {
 
   const course = courses.find((c) => c.id === id);
   res.send(course);
+});
+
+app.get("/faq", (req, res) => {
+  res.send(faq);
 });
 
 app.listen(port, () => {
